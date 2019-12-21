a37393a8d94999b11aaa3d3981bc696776489c38
Samuel <fg.samuel@gmail.com>
2019-12-21 20:36:45 -0300

    Atualização

--- exemplo_bootstrap/settings.py
+++ exemplo_bootstrap/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'exemplo_bootstrap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
